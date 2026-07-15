0c3c3e3f50f040bff6120d454363e496510d005a
adamsissac-raj <adamsissacraj2003@gmail.com>
2026-07-15 13:28:56 +0530

    Add files via upload

---
+++ Area_of_Rectangle.java
@@ -0,0 +1,15 @@
+package Chapter_1;
+
+public class Area_of_Rectangle 
+{
+
+	public static void main(String[] args) 
+	{
+		int l=10,w=5;
+		int area=  l*w;
+		System.out.println("Length : " +l);
+		System.out.println("Width : " +w);
+		System.out.println("Area of Rectangle : " +area);
+	}
+
+}
