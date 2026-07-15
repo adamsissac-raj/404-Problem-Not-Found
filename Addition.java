a840e5aedac8262322b15248b33d1230a7b7d8a5
adamsissac-raj <adamsissacraj2003@gmail.com>
2026-07-15 13:27:49 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,15 @@
+package Chapter_1;
+
+public class Addition 
+{
+
+	public static void main(String[] args) 
+	{
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("a = " +a);
+		System.out.println("b = " +b);
+		System.out.println("a+b= " +c);
+	}
+}
