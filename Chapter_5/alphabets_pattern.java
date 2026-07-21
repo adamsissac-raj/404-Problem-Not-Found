6822af47c8f10ba6f962662ab8511598c7a73ad3
adamsissac-raj <adamsissacraj2003@gmail.com>
2026-07-21 13:05:01 +0530

    Add files via upload

---
+++ Chapter_5/alphabets_pattern.java
@@ -0,0 +1,19 @@
+package Chapter_5;
+
+public class alphabets_pattern
+{
+
+	public static void main(String[] args) 
+	{
+		for (int i=65;i<70;i++)
+		{
+			for (int j=65;j<=i;j++)
+			
+				{
+					char ch=(char)j;
+					System.out.print(ch + " ");
+				}
+			System.out.println();
+		}
+	}
+}
---
+++ Chapter_5/diagonal_star.java
@@ -0,0 +1,26 @@
+package Chapter_5;
+
+public class diagonal_star 
+{
+
+	public static void main(String[] args) 
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=5;j++)
+			{
+				if(i==j)
+				{
+				System.out.print("* ");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println("");
+		}
+		System.out.println("Completed");
+	}
+
+}
